abfe484a6fb49ccc4f22f719a47e33f84c5fbc34
Markos Ayala <elmarkos23@gmail.com>
2019-03-14 12:25:28 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZXing;
+
+namespace WinForms.CodigoBarras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instalar nuget
+            // Install-Package ZXing.Net 
+
+            //codigo
+            string strCodigo = "1911201301110302914400110010010000000041234567810";
+
+            // instantiate a writer object
+            var barcodeWriter = new BarcodeWriter();
+
+            // set the barcode format
+            barcodeWriter.Format = BarcodeFormat.CODE_39;
+
+            // write text and generate a 2-D barcode as a bitmap
+            barcodeWriter.Write(strCodigo).Save(@"D:\\"+ strCodigo + ".bmp");
+        }
+    }
+}
